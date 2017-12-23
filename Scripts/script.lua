d84435a0e571fdb60ddd88965cbc806b4af2236e
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-12-23 22:32:36 +0100

    add snowman

--- Scripts/script.lua
+++ Scripts/script.lua
@@ -47,31 +47,36 @@ function Create_Christmas_Presents()
 
     local color_gifts = {'blue', 'green', 'green2', 'pink', 'red', 'yellow','snowflake', 'owl', 'star'}
     local new_props = {}
+    new_props[table.getn(new_props)+ 1] = '/mods/santa_is_coming_reloaded/props/snowman/snowman_prop.bp'
     for _, color in color_gifts do
         new_props[table.getn(new_props)+1] = '/mods/santa_is_coming_reloaded/props/gift_' .. color .. '/gift_' .. color .. '_prop.bp'
     end
     for k = 1, 3 do
         new_props[table.getn(new_props)+ 1] = '/mods/santa_is_coming_reloaded/props/cookie/cookie_prop.bp'
     end
+    
     for _, present in ListOfProps or {} do
+    local proptype = math.ceil(math.random(table.getn(new_props)))
         local NewGift = CreateProp( VECTOR3(present:GetPosition()['x'],
                             present:GetPosition()['y'],
                             present:GetPosition()['z'] ),
-                            new_props[math.ceil(math.random(table.getn(new_props)))])
+                            new_props[proptype])
                             
         -- set the reclaim values to match the removed prop
         NewGift:SetMaxReclaimValues( 5, present:GetBlueprint().Economy.ReclaimMassMax, present:GetBlueprint().Economy.ReclaimEnergyMax)
         -- determine the new size of the prop
         local new_size = ( (present:GetBlueprint().Economy.ReclaimMassMax + present:GetBlueprint().Economy.ReclaimEnergyMax/10 + math.random(25)) )   -- + 
         -- map the size of the prop into the allowed range        
---        new_size = 0.02 + (0.084-0.02)*(new_size/600-0.005)/(0.23-0.005)
         new_size = 0.02 + 0.064*(new_size-3)/135
         if(new_size < 0.02) then
             new_size = 0.02
         elseif (new_size > 0.084) then
             new_size = 0.084
         end
-        NewGift:SetScale(new_size)
+        if proptype == 1 then
+            new_size = 100 * new_size
+        end
+        NewGift:SetScale(new_size*0.7)
         
         local orient = math.random(628)/100-3.14
         local vec = VECTOR3(math.cos(orient),0,math.sin(orient))
