5e7ff4179c4b813824ba2dee4008b15926fff173
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-12-23 22:32:43 +0100

    bump version

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "Santa is Coming Reloaded"
-uid = "Santa-is-comi-ng03-90f1-0264a2479872"
-version = 3
+uid = "Santa-is-comi-ng06-90f1-0264a2479872"
+version = 6
 copyright = "CookieNoob and Ozonex"
 description = "Santa is coming reloaded (with cookies). The mod transforms some of the rocks and trees into christmas gifts. The size of the gifts depends on the amount of reclaim in it. Maps with fewer trees/rocks will convert a higher fraction to gifts. Thanks to Black_Master for the GC model."
 author = "CookieNoob (script) and Ozonex (models)"
