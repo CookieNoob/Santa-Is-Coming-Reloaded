6c843908c1ee8626ef78c9a4bca3b8842239c0a0
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-12-25 11:24:57 +0100

    make santa hat visible for enemy team when in vision range

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "Santa is Coming Reloaded"
-uid = "Santa-is-comi-ng08-90f1-0264a2479872"
-version = 8
+uid = "Santa-is-comi-ng10-90f1-0264a2479872"
+version = 10
 copyright = "CookieNoob and Ozonex"
 description = "Santa is coming reloaded (with cookies). The mod transforms some of the rocks and trees into christmas gifts. The size of the gifts depends on the amount of reclaim in it. Maps with fewer trees/rocks will convert a higher fraction to gifts. Thanks to Black_Master for the GC model."
 author = "CookieNoob (script) and Ozonex (models)"
